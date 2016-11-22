4696f93d858e7fa29a0669f922d2825983316557
DGVenkatesh <dgvenkatesh08@gmail.com>
2016-11-22 21:14:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
       database: 'dgvenkatesh',
       host: 'db.imad.hasura-app.io',
       port: '5432',
-      password: process.env.PASS
+      password: process.env.DB_PASSWORD
 };
 
 var app = express();
