f7b3349e70f09b6783c4d83c54035697601c4048
DGVenkatesh <dgvenkatesh08@gmail.com>
2016-11-22 20:32:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/counter',function (req, res)
    res.send(counter.toString());
 });
 
-app.get('/article:articleName', function (req, res){
+app.get('/article/:articleName', function (req, res){
      pool.query("SELECT *FROM article WHERE title = ' " + req.params.articleName +" '", function (err, result){
         if (err){
             res.status(500).send(err.toString());
