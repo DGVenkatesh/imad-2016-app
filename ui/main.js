44196740758d35c8f44dbf42b38dfe0b4c759211
DGVenkatesh <dgvenkatesh08@gmail.com>
2016-11-26 22:25:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,26 @@ function fun() {
     alert ("Created Sucessfully");
     alert ("Enter Username-user001, Password-12345");
 }
+var submit = document.getElementById('sub');
+submit.onclick = function () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                alert ();
+                
+            } else if (request.status === 403){
+                alert('username password incorrect');
+            }
+        }
+    }; 
+    
+    var username = document.getElementbyId('username').value;
+    var password = document.getElementbyId('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://dgvenkatesh.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
+
