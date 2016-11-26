8fb3782f0e463b02bb8e15bcc9b3d5c2de69b0f1
DGVenkatesh <dgvenkatesh08@gmail.com>
2016-11-26 09:02:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,3 @@
-var button = document.getElementById("counter");
-var counter =0;
-button.onclick = function () {
-  
-  
-  
-  
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-};
+fun(); {
+    alert ("Creted Sucessfully Username-user001 Password -12345");
+}
