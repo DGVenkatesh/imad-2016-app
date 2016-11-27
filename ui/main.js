dad261786d7d3651301186c7cf6c20be482b3205
DGVenkatesh <dgvenkatesh08@gmail.com>
2016-11-27 08:30:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++
@@ -1,7 +0,0 @@
-function fun() {
-    alert ("Created Sucessfully");
-    alert ("Enter Username-user001, Password-12345");
-}
-function sub(){
-   window.open("https://www.youtube.com/user/NewWorldOps");
-}
